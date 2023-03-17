6f195391433001f8253693f3fb665f4ed1c76481
Muxammadbobur <bobur0172@gmail.com>
2023-03-17 08:37:54 -0700

    first commit

--- index.js
+++ index.js
@@ -80,6 +80,6 @@ app.get("/", async (req, res) => {
   return res.status(200).json({ message: "Success" });
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log("server started on PORT" + PORT));
